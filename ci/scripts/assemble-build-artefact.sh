9d9de7bea5b10086884fe944af3ee60b40913039
Chris Whitty <1538281+crswty@users.noreply.github.com>
2019-09-12 16:50:44 +0100

    Change order of args for tar

--- ci/scripts/assemble-build-artefact.sh
+++ ci/scripts/assemble-build-artefact.sh
@@ -2,5 +2,5 @@
 
 mkdir archive
 tar -czf archive/projects.tgz \
-  marketplace-project/config/crd/bases/marketplace.pivotal.io_projects.yaml \
-  -C marketplace-project/config/crd/bases
+  -C marketplace-project/config/crd/bases \
+  marketplace-project/config/crd/bases/marketplace.pivotal.io_projects.yaml

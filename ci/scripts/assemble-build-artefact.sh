fd36404629c3b88b0977f5c6b2fdba71ff3c6caf
Chris Whitty <1538281+crswty@users.noreply.github.com>
2019-09-12 16:58:08 +0100

    fix path

--- ci/scripts/assemble-build-artefact.sh
+++ ci/scripts/assemble-build-artefact.sh
@@ -3,4 +3,4 @@
 mkdir archive
 tar -czf archive/projects.tgz \
   -C marketplace-project/config/crd/bases \
-  marketplace-project/config/crd/bases/marketplace.pivotal.io_projects.yaml
+  marketplace.pivotal.io_projects.yaml

6505c08cafe8f51d171b92ac05d53689b4256f2f
Chris Whitty <1538281+crswty@users.noreply.github.com>
2019-09-12 16:43:47 +0100

    Remove deployment.yaml for controller and trim leading path in archive

--- ci/scripts/assemble-build-artefact.sh
+++ ci/scripts/assemble-build-artefact.sh
@@ -3,4 +3,4 @@
 mkdir archive
 tar -czf archive/projects.tgz \
   marketplace-project/config/crd/bases/marketplace.pivotal.io_projects.yaml \
-  marketplace-project/deployment/deployment.yaml
+  -C marketplace-project/config/crd/bases

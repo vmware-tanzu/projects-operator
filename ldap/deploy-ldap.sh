1b024e44898897e43c998ed72cd1fc9181b60fd0
Ed King <eking@pivotal.io>
2019-09-19 15:47:47 +0100

    Clean up ldap deploy and user generation scripts
    [#167615091]
    
    Co-authored-by: James Myers <jmyers@pivotal.io>

--- ldap/deploy-ldap.sh
+++ ldap/deploy-ldap.sh
@@ -1,37 +1,43 @@
 #!/usr/bin/env bash
-
-kubectl get secret openldap-admin-secret 1>/dev/null 2>&1
-secret_exists=$?
-
 set -eu
 
+NAMESPACE=openldap
+ADMIN_SECRET_NAME=openldap-admin-secret
+
+kubectl create ns $NAMESPACE
+
+absolute_path() {
+  (cd "$1" && pwd)
+}
+scripts_path=$(absolute_path "$(dirname "$0")" )
+
+set +e
+kubectl -n $NAMESPACE get secret $ADMIN_SECRET_NAME 1>/dev/null 2>&1
+secret_exists=$?
+set -e
 
 if [ $secret_exists -eq 0 ]
 then
-  echo "openldap-admin-secret Secret already exists, reusing"
-  adminPassword="$(kubectl get secret openldap-admin-secret -o json | jq -r '.data.password' | base64 --decode)"
+  echo "$ADMIN_SECRET_NAME secret already exists, reusing"
+  adminPassword="$(kubectl -n $NAMESPACE get secret $ADMIN_SECRET_NAME -o json | jq -r '.data.password' | base64 --decode)"
 else
-  echo "openldap-admin-secret Secret not found, creating"
+  echo "$ADMIN_SECRET_NAME Secret not found, creating"
   adminPassword="$(cat /dev/urandom | env LC_CTYPE=C tr -dc a-z | head -c 32)"
 
-cat <<EOF | kubectl create -f -
+cat <<EOF | kubectl -n $NAMESPACE create -f -
 apiVersion: v1
 kind: Secret
 metadata:
-  name: openldap-admin-secret
+  name: $ADMIN_SECRET_NAME
 type: Opaque
 data:
   password: "$(echo $adminPassword | base64)"
 EOF
-
 fi
 
-
-kubectl apply -f storage-class-gcp.yml
-
-kubectl apply -f open-ldap-deployment.yaml
-kubectl apply -f php-ldap-admin-deployment.yaml
-
+kubectl -n $NAMESPACE apply -f ${scripts_path}/storage-class-gcp.yml
+kubectl -n $NAMESPACE apply -f ${scripts_path}/open-ldap-deployment.yaml
+kubectl -n $NAMESPACE apply -f ${scripts_path}/php-ldap-admin-deployment.yaml
 
 echo
 echo "Admin Credentials:"
@@ -40,4 +46,4 @@ echo "Password: $adminPassword"
 
 echo
 echo "Once the service has a external IP assigned, to populate data, you can run:"
-echo "    ./generate-users.sh <external-ip> cn=admin,dc=example,dc=com $adminPassword"
+echo "    ${scripts_path}/generate-users.sh <external-ip> cn=admin,dc=example,dc=com $adminPassword"
--- ldap/generate-users.sh
+++ ldap/generate-users.sh
@@ -1,24 +1,27 @@
 #!/usr/bin/env bash
 
+set -eu
+
 host=$1
 dn=$2
 password=$3
 
-kubectl get secret openldap-user-secret 1>/dev/null 2>&1
+NAMESPACE=openldap
+
+set +e
+kubectl -n $NAMESPACE get secret openldap-user-secret 1>/dev/null 2>&1
 secret_exists=$?
-
-set -eu
-
+set -e
 
 if [ $secret_exists -eq 0 ]
 then
   echo "openldap-user-secret Secret already exists, reusing"
-  userPassword="$(kubectl get secret openldap-user-secret -o json | jq -r '.data.password' | base64 --decode)"
+  userPassword="$(kubectl -n $NAMESPACE get secret openldap-user-secret -o json | jq -r '.data.password' | base64 --decode)"
 else
   echo "openldap-user-secret Secret not found, creating"
   userPassword="$(cat /dev/urandom | env LC_CTYPE=C tr -dc a-z | head -c 32 | base64)"
 
-cat <<-EOF | kubectl apply -f -
+cat <<-EOF | kubectl -n $NAMESPACE apply -f -
 apiVersion: v1
 kind: Secret
 metadata:
@@ -27,60 +30,37 @@ type: Opaque
 data:
   password: $userPassword
 EOF
-
 fi
-
 
 hashedPassword="$(slappasswd -s $userPassword -h {SSHA})"
 
 ldapadd -c -h $host -D $dn -w $password <<EOF
 
-dn: cn=posix-group,dc=example,dc=com
-cn: posix-group
-gidnumber: 500
-objectclass: posixGroup
-objectclass: top
-
 dn: cn=cody,dc=example,dc=com
 cn: cody
-gidnumber: 500
 givenname: Enterprise Resource
-homedirectory: /home/users/cody
 mail: cody@example.com
 objectclass: inetOrgPerson
-objectclass: posixAccount
 objectclass: top
 sn: Unit
-uid: cody
-uidnumber: 1000
 userpassword: $hashedPassword
 
 dn: cn=alice,dc=example,dc=com
 cn: alice
-gidnumber: 500
 givenname: Enterprise Resource
-homedirectory: /home/users/alice
 mail: alice@example.com
 objectclass: inetOrgPerson
-objectclass: posixAccount
 objectclass: top
 sn: Unit
-uid: alice
-uidnumber: 1000
 userpassword: $hashedPassword
 
 dn: cn=bob,dc=example,dc=com
 cn: bob
-gidnumber: 500
 givenname: Enterprise Resource
-homedirectory: /home/users/bob
 mail: bob@example.com
 objectclass: inetOrgPerson
-objectclass: posixAccount
 objectclass: top
 sn: Unit
-uid: bob
-uidnumber: 1000
 userpassword: $hashedPassword
 
 dn: cn=ldap-experts,dc=example,dc=com
@@ -88,9 +68,8 @@ cn: ldap-experts
 member: cn=cody,dc=example,dc=com
 objectclass: groupOfNames
 objectclass: top
-
 EOF
 
 echo
 echo "Users created:"
-echo "user: eunit password: $userPassword"
+echo "user: user password: $userPassword"

dd222b1ef4ba6cd0ef8c01876ec70eeedf3689c1
Ed King <eking@pivotal.io>
2019-09-20 10:49:42 +0100

    Add version to tgz artefact
    [#168429575]
    
    Co-authored-by: Ed Cook <ecook@pivotal.io>

--- ci/scripts/assemble-build-artefact.sh
+++ ci/scripts/assemble-build-artefact.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 mkdir archive
-tar -czf archive/projects.tgz \
+tar -czf archive/projects-$(cat version/number).tgz \
   -C marketplace-project/config/crd/bases \
   marketplace.pivotal.io_projects.yaml

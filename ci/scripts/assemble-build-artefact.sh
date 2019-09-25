4ab8d916cf8d5b18fc9b3377119d3ea52aacedf3
Alberto Leal <aleal@pivotal.io>
2019-09-25 13:54:36 +0100

    Projects artefact contains all kustomize config
    [#167893103]
    
    Co-authored-by: Sam Gunaratne <sgunaratne@pivotal.io>

--- ci/scripts/assemble-build-artefact.sh
+++ ci/scripts/assemble-build-artefact.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
 mkdir archive
-tar -czf archive/projects-$(cat version/number).tgz \
-  -C marketplace-project/config/crd/bases \
-  marketplace.pivotal.io_projects.yaml
+tar -czf archive/projects-$(cat version/number).tgz marketplace-project/config

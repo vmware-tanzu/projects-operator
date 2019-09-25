2030175dca0e67d8169dc77ac216dd3a5107fda2
Alberto Leal <aleal@pivotal.io>
2019-09-25 14:17:08 +0100

    Project config should be top level dir in tgz
    [#167893103]
    
    Co-authored-by: Sam Gunaratne <sgunaratne@pivotal.io>

--- ci/scripts/assemble-build-artefact.sh
+++ ci/scripts/assemble-build-artefact.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 mkdir archive
-tar -czf archive/projects-$(cat version/number).tgz marketplace-project/config
+tar -czf archive/projects-$(cat version/number).tgz -C marketplace-project config

fe12c1fcb2868689715fe9841aaf01675773482a
Chris Whitty <cwhitty@pivotal.io>
2019-10-09 15:54:02 +0100

    Move to using a single value for image so image relocation will work
    Co-authored-by: Alberto Leal <aleal@pivotal.io>

--- ci/scripts/assemble-build-artefact.sh
+++ ci/scripts/assemble-build-artefact.sh
@@ -4,4 +4,5 @@
 VERSION=$(cat version/number)
 sed -i s/version:.*/version:\ ${VERSION}/ marketplace-project/helm/projects-operator/Chart.yaml
 sed -i s/appVersion:.*/appVersion:\ ${VERSION}/ marketplace-project/helm/projects-operator/Chart.yaml
+sed -i s/image:.*/image:\ gcr.io/cf-ism-0/marketplace-project:${VERSION}/ marketplace-project/helm/projects-operator/values.yaml
 helm package marketplace-project/helm/projects-operator/ -d archive/

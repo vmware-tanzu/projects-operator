644622e35efda579892f6dab031fbf35aa2d4203
Ed King <eking@pivotal.io>
2019-10-21 11:55:54 +0100

    Refactor out tasks into separate yml files

--- ci/scripts/assemble-build-artefact.sh
+++ ci/scripts/assemble-build-artefact.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-
 VERSION=$(cat version/number)
+
 sed -i s/version:.*/version:\ ${VERSION}/ marketplace-project/helm/projects-operator/Chart.yaml
 sed -i s/appVersion:.*/appVersion:\ ${VERSION}/ marketplace-project/helm/projects-operator/Chart.yaml
 sed -i s/image:.*/image:\ gcr.io/cf-ism-0/marketplace-project:${VERSION}/ marketplace-project/helm/projects-operator/values.yaml
+
 helm package marketplace-project/helm/projects-operator/ -d archive/

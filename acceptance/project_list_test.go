6160b912e40f935ccb0f3afc06757dd9cae1f31d
Ed Cook <ecook@pivotal.io>
2020-01-29 16:36:30 +0000

    Update project_list acceptance tests in line with implementation
    [#170129412]

--- acceptance/project_list_test.go
+++ acceptance/project_list_test.go
@@ -10,12 +10,16 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = PDescribe("ProjectAccess CRD", func() {
+var _ = Describe("ProjectAccess CRD", func() {
 	var (
 		// Note that these users have a corresponding identity in LDAP
 		// see the README.md for further info
 		alana testhelpers.KubeActor // an "admin/operator"
 		cody  testhelpers.KubeActor // a "developer"
+
+		projectName       string
+		projectAccessName string
+		projectResource   string
 	)
 
 	BeforeEach(func() {
@@ -24,74 +28,55 @@ var _ = PDescribe("ProjectAccess CRD", func() {
 		codyToken := GetToken(Params.UaaLocation, "cody", Params.CodyPassword)
 		cody = testhelpers.NewKubeActor("cody", codyToken)
 
-		// grant cody ability to crud projectaccesses
-		projectAccessClusterRole := `
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRole
-metadata:
-  name: "cody-projectaccess"
-rules:
-- apiGroups:
-  - developerconsole.pivotal.io
-  resources:
-  - projectaccesses
-  verbs:
-  - "*"
-`
+		projectName = fmt.Sprintf("my-project-%d", time.Now().UnixNano())
+		projectAccessName = fmt.Sprintf("my-projectaccess-%d", time.Now().UnixNano())
 
-		projectAccessClusterRoleBinding := `
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRoleBinding
-metadata:
-  name: "cody-projectaccess"
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: cody-projectaccess
-subjects:
-- apiGroup: rbac.authorization.k8s.io
-  kind: User
-  name: cody
-`
+		projectResource = fmt.Sprintf(`
+                apiVersion: developerconsole.pivotal.io/v1alpha1
+                kind: Project
+                metadata:
+                 name: %s
+                spec:
+                  access:
+                  - kind: User
+                    name: %s`, projectName, userName("cody"))
 
-		// TODO: clean this up
-		alana.MustRunKubectl("apply", "-f", AsFile(projectAccessClusterRole))
-		alana.MustRunKubectl("apply", "-f", AsFile(projectAccessClusterRoleBinding))
+		alana.MustRunKubectl("apply", "-f", AsFile(projectResource))
 	})
 
 	AfterEach(func() {
-		alana.MustRunKubectl("delete", "projectaccess", "cody-project-access")
+		alana.MustRunKubectl("delete", "projectaccess", projectAccessName)
+
+		alana.MustRunKubectl("delete", "-f", AsFile(projectResource))
 	})
 
 	It("can be created", func() {
-		projectAccessResource := `
+		projectAccessResource := fmt.Sprintf(`
                 apiVersion: developerconsole.pivotal.io/v1alpha1
                 kind: ProjectAccess
                 metadata:
-                  name: cody-project-access`
+                  name: %s`, projectAccessName)
 
 		_, err := alana.RunKubeCtl("apply", "-f", AsFile(projectAccessResource))
 		Expect(err).NotTo(HaveOccurred())
 	})
 
 	It("eventually has its status updated to include a list of projects cody has access to", func() {
-		projectAccessResource := `
+		projectAccessResource := fmt.Sprintf(`
                 apiVersion: developerconsole.pivotal.io/v1alpha1
                 kind: ProjectAccess
                 metadata:
-                  name: cody-project-access`
+                  name: %s`, projectAccessName)
 
 		_, err := cody.RunKubeCtl("apply", "-f", AsFile(projectAccessResource))
 		Expect(err).NotTo(HaveOccurred())
 
-		//TODO: greatly improve this part of the test
 		Eventually(func() string {
-			output, err := cody.RunKubeCtl("get", "projectaccess", "cody-project-access", "-o", "yaml")
+			output, err := cody.RunKubeCtl("get", "projectaccess", projectAccessName, "-o", "yaml")
 			if err != nil {
 				fmt.Fprint(GinkgoWriter, err.Error())
 			}
-
 			return output
-		}, time.Second*3).Should(ContainSubstring("ldap-experts"))
+		}, time.Second*3).Should(ContainSubstring(projectName))
 	})
 })

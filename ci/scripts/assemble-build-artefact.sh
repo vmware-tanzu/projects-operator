2775ef6e3fe26c0cd288be2ee39298628034b3b3
Ed King <eking@vmware.com>
2020-06-16 15:53:03 +0100

    Fix path in ci assemble script

--- ci/scripts/assemble-build-artefact.sh
+++ ci/scripts/assemble-build-artefact.sh
@@ -4,6 +4,6 @@ VERSION=$(cat version/version)
 
 sed -i s/version:.*/version:\ ${VERSION}/ projects-operator/helm/projects-operator/Chart.yaml
 sed -i s/appVersion:.*/appVersion:\ ${VERSION}/ projects-operator/helm/projects-operator/Chart.yaml
-sed -i "s/tag: latest/tag: ${VERSION}/" ~/workspace/projects-operator/helm/projects-operator/values.yaml
+sed -i "s/tag: latest/tag: ${VERSION}/" projects-operator/helm/projects-operator/values.yaml
 
 helm package projects-operator/helm/projects-operator/ -d archive/

89360cf20bb45b0f3316e35ca581e023c67d6bf6
Danny Joyce <djoyce@pivotal.io>
2020-07-09 14:32:10 -0400

    Add docker config for kbld auth

--- ci/scripts/kapp-deploy.sh
+++ ci/scripts/kapp-deploy.sh
@@ -7,6 +7,18 @@ if [ -n "$KUBECONFIG_FILE_CONTENTS" ]; then
     echo "$KUBECONFIG_FILE_CONTENTS" > "$HOME/.kube/config"
 fi
 
+mkdir -p $HOME/.docker
+DOCKER_AUTH=$(echo -n "$REGISTRY_USERNAME:$REGISTRY_PASSWORD" | base64 - | tr -d '\n')
+cat <<EOT > $HOME/.docker/config.json
+{
+  "auths": {
+    "${REGISTRY_URL}": {
+        "auth": "${DOCKER_AUTH}"
+    }
+  }
+}
+EOT
+
 # install kapp (TODO: put in the base ci image)
 wget "https://github.com/k14s/kapp/releases/download/v0.30.0/kapp-linux-amd64" -O "/usr/local/bin/kapp"
 chmod u+x "/usr/local/bin/kapp"

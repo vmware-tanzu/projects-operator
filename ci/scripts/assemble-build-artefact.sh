2f265b46b9977798b9f1002abcbc8d36d72c904a
Danny Joyce <djoyce@pivotal.io>
2020-07-09 17:35:46 -0400

    Fix tar for linux

--- ci/scripts/assemble-build-artefact.sh
+++ ci/scripts/assemble-build-artefact.sh
@@ -4,5 +4,5 @@ VERSION=$(cat version/version)
 
 sed -i "s/version:.*/version:\ ${VERSION}/" projects-operator/deployments/k8s/values.yaml
 
-tar -czf projects-operator-${VERSION}.tgz -C projects-operator/deployments -s /k8s/projects-operator/ k8s/
+tar -czf projects-operator-${VERSION}.tgz -C projects-operator/deployments --transform "s/k8s/projects-operator/g" k8s/
 mv projects-operator-${VERSION}.tgz archive/projects-operator-${VERSION}.tgz

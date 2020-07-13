17598fc7897ec2c22cf5d33db946101f087537d0
Danny Joyce <djoyce@pivotal.io>
2020-07-13 10:12:47 -0400

    Remove kapp and kbld installation from ci script

--- ci/scripts/kapp-deploy.sh
+++ ci/scripts/kapp-deploy.sh
@@ -19,14 +19,6 @@ cat <<EOT > $HOME/.docker/config.json
 }
 EOT
 
-# install kapp (TODO: put in the base ci image)
-wget "https://github.com/k14s/kapp/releases/download/v0.30.0/kapp-linux-amd64" -O "/usr/local/bin/kapp"
-chmod u+x "/usr/local/bin/kapp"
-
-# install kbld (TODO: put in the base ci image)
-wget "https://github.com/k14s/kbld/releases/download/v0.23.0/kbld-linux-amd64" -O "/usr/local/bin/kbld"
-chmod u+x "/usr/local/bin/kbld"
-
 VERSION=$(cat version/version)
 
 cd projects-operator

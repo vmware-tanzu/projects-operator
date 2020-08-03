08053697f9bb56bdb1f4393519af73dbb82309cf
Ed Cook <ecook@vmware.com>
2020-08-03 14:48:34 +0100

    When changing version, use the new values file
    [#173681350]
    
    Signed-off-by: Glen Rodgers <grodgers@vmware.com>

--- ci/scripts/assemble-build-artefact.sh
+++ ci/scripts/assemble-build-artefact.sh
@@ -2,7 +2,7 @@
 
 VERSION=$(cat version/version)
 
-sed -i "s/version:.*/version:\ ${VERSION}/" projects-operator/deployments/k8s/values.yaml
+sed -i "s/version:.*/version:\ ${VERSION}/" projects-operator/deployments/k8s/values/_default.yaml
 
 tar -czf projects-operator-${VERSION}.tgz -C projects-operator/deployments --transform "s/k8s/projects-operator/g" k8s/
 mv projects-operator-${VERSION}.tgz archive/projects-operator-${VERSION}.tgz

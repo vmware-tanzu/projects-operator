0960faf6e108666bc1953f5195abbe4489b8ecfa
Glen Rodgers <grodgers@vmware.com>
2020-08-03 14:52:17 +0100

    Improve bashing in scripts
    Signed-off-by: Ed Cook <ecook@vmware.com>

--- ci/scripts/assemble-build-artefact.sh
+++ ci/scripts/assemble-build-artefact.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 VERSION=$(cat version/version)
 
-sed -i "s/version:.*/version:\ ${VERSION}/" projects-operator/deployments/k8s/values/_default.yaml
+sed -i "s/version:.*/version:\ ${VERSION}/" "projects-operator/deployments/k8s/values/_default.yaml"
 
-tar -czf projects-operator-${VERSION}.tgz -C projects-operator/deployments --transform "s/k8s/projects-operator/g" k8s/
-mv projects-operator-${VERSION}.tgz archive/projects-operator-${VERSION}.tgz
+tar -czf "projects-operator-${VERSION}.tgz" -C "projects-operator/deployments" --transform "s/k8s/projects-operator/g" "k8s/"
+mv "projects-operator-${VERSION}.tgz" "archive/projects-operator-${VERSION}.tgz"
